ccd5d1d5879dfac9e9e18ee87cfd7c8cdb43eda7
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-07-10 19:08:21 +0200

    switch case

--- js06_Functions2/app.js
+++ js06_Functions2/app.js
@@ -15,19 +15,47 @@ console.log("***Arrow Function ***")
 
 //? Eksempel 1
 
-const calculateAge = ( year) => new Date().getFullYear() - year
-console.log(`AGe:${calculateAge(1990)}`)
+// const calculateAge = ( year) => new Date().getFullYear() - year
+// console.log(`AGe:${calculateAge(1990)}`)
 
 //? Eksempel 2  
 
 //* volume = Pi x r x r x h
 
-const h= Number (prompt("h:"))
-const r= Number (prompt("r:"))
+// const h= Number (prompt("h:"))
+// const r= Number (prompt("r:"))
 
 
-const volume= (r, h) => Math.PI * r * r * h
+// const volume= (r, h) => Math.PI * r * r * h
 
 
-console.log("Volume:" , volume(r, h).toFixed(2))
-console.log("Volume:" , volume(r, h).toPrecision(5)) // * kac basamakli istersek
+// console.log("Volume:" , volume(r, h).toFixed(2))
+// console.log("Volume:" , volume(r, h).toPrecision(5)) // * kac basamakli istersek
+
+
+//* Eksempel 3  Calculator  birdern fazla durumlarda switch case durumu 
+
+const compute = (n1, n2, operator) => {
+let result = 0
+    switch (operator) {
+        case "+":
+       // result = n1 + n2
+        result = add(n1, n2)
+            
+            break;
+        case "-":
+            
+            break;
+    
+        default:
+            break;
+    }
+
+}
+
+
+const n1 = + prompt("First Number:")
+const n2 = + prompt("Second Number:")
+const operator= + prompt("Operator:")
+
+console.log(compute(n1, n2, operator))
