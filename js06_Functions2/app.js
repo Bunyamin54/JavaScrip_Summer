66f0025bb7cd6b838aca18cf29e44f9f033db0f8
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-07-10 18:53:32 +0200

    pi volume calculata eksempel function arrow

--- js06_Functions2/app.js
+++ js06_Functions2/app.js
@@ -17,3 +17,16 @@ console.log("***Arrow Function ***")
 
 const calculateAge = ( year) => new Date().getFullYear() - year
 console.log(`AGe:${calculateAge(1990)}`)
+
+//? Eksempel 2  
+
+//* volume = Pi x r x r x h
+
+const h= Number (prompt("h:"))
+const r= Number (prompt("r:"))
+
+
+const volume= (radius, height) => Math.PI * radius * r * height
+
+
+console.log("Volume:" , volume(r, h))
