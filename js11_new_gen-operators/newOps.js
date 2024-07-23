854f659a30b42cdff6b9302bf8e36fb08eb098c9
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-07-23 14:32:40 +0200

    new operations

--- js11_new_gen-operators/newOps.js
+++ js11_new_gen-operators/newOps.js
@@ -220,9 +220,11 @@
 
   console.log(tasitlar1)
 
+ 
+
   const cumle = "Olmak yada olmamak"
 
-  const karakter = [...cumle]
+    const karakter = [...cumle]
 
-  console.log(karakter)
-  console.log(cumle)
+    console.log(karakter)
+    console.log(cumle)
