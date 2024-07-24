f6defac7a2b4f7c24ffffdc7cd2325f545eabf0a
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 15:42:57 +0200

    logical operators

--- js03_conditions/app.js
+++ js03_conditions/app.js
@@ -19,29 +19,48 @@ console.log("*** Conditions ***")
 // //? cok kosullu
 
 
-const grade = prompt ("Plase enter your grade:")
+// const grade = prompt ("Plase enter your grade:")
 
 
-let result = ""
+// let result = ""
 
-if (grade < 0) {
-    console.log("Grade can not be small than zero")
+// if (grade < 0) {
+//     console.log("Grade can not be small than zero")
+// }
+
+// else if (grade < 40  ) {
+//     result = "FF"
+// }else if (grade <=50) {
+//     result= "DD"
+// }else if (grade <= 65){
+//     result="CC"
+// }else if (grade <=89) {
+//     result="BB"
+// }else if (grade <=100 ){
+//     result="AA"
+// }else {
+//     console.log("Grade can not be bigger than 100")
+// }
+
+// if (result != "") {
+//     console.log(`Your score: ${result}`)
+// }
+
+
+//? ornek 
+
+
+const n1 = +prompt("Num1:")
+const n2 = +prompt("Num2:")
+const n3 = +prompt("Num3:")
+
+if ( n1 > n2){
+    if(n1 > n3) {
+        console.log("Biggest:", n1)
+    }
 }
 
-else if (grade < 40  ) {
-    result = "FF"
-}else if (grade <=50) {
-    result= "DD"
-}else if (grade <= 65){
-    result="CC"
-}else if (grade <=89) {
-    result="BB"
-}else if (grade <=100 ){
-    result="AA"
-}else {
-    console.log("Grade can not be bigger than 100")
-}
 
-if (result != "") {
-    console.log(`Your score: ${result}`)
+if (n1 >  n2 && n1 > n3) {
+    console.log("Biggest:", n1)
 }
