8753d0e4331bc174c01ae4b35615064724dce6dc
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 15:59:29 +0200

    estimation tids

--- js06_Functions2/recursive.js
+++ js06_Functions2/recursive.js
@@ -4,3 +4,7 @@
 //*  Kendi kendini cagiran fonksiyona recursive function denir 
 
 //? Finonaci terms  n! = n * (n-1)!
+
+// * fibonaci kendinden oncekini topla 1 -1 -2 3- 5- 8- 13- 21  
+
+//* agile ticket login.js  - estimation tid
