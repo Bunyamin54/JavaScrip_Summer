bc50fc55dbd763ef399bbabc7258ce965c2ce47b
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 15:49:31 +0200

    function expression

--- js05_Functions/expression.js
+++ js05_Functions/expression.js
@@ -3,9 +3,10 @@
 
 //* Anonymous
 
+//* Declarasyon 
 
 const yazdir = function(message) {
     console.log(message)
 }
 
-yazdir("Hallo FS14")  //? invoke
+yazdir("Hallo FS14")  //? invoke  funtion can be global whic is not so useful
