e04ee26fbc425b0e4d47e1e8b41fbc52ac4ee29f
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 15:46:29 +0200

    local - global fornaring med for

--- js04_loops/app.js
+++ js04_loops/app.js
@@ -2,13 +2,28 @@
 
  console.log("***** Lopps*****")
 
-for (let i = 1; i <=99; i++) {
+// for (let i = 1; i <=99; i++) {
 
-    console.log(`${i}- FS-14`)
+//     console.log(`${i}- FS-14`)
+// }
+
+
+// for( let i =1 ; i<= 10; i++) {
+
+//     console.log("Selam")
+// }
+
+const n = prompt("Enter your number")
+
+let sum = 0  //* global degisken
+
+for (let i = 1 ; i<= n ; i++) {
+
+
+    //! lokal degisken
+    // sum = sum + 1
+    sum += i //* kisa hali
+    console.log(sum)
 }
 
-
-for( let i =1 ; i<= 10; i++) {
-
-    console.log("Selam")
-}
+console.log(`SUM: ${sum}`)
