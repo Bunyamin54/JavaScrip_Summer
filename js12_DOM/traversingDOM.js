ea03afc8edbe6124fc7a060c56a73543d2c8977b
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 22:27:40 +0200

    parentNode

--- js12_DOM/traversingDOM.js
+++ js12_DOM/traversingDOM.js
@@ -2,7 +2,7 @@
 
 console.log("Traversing DOM")
 
-//* Traversing DOM 
+//* Traversing DOM   bir yerden baska yere gitme
 
 //? bir html elementini uzerinden baska elementleri secemeye Dom traversing denilir. 
 //? Secme islemi 3 yonlu yapilir   
@@ -10,3 +10,12 @@ console.log("Traversing DOM")
 //? Yukari yonde (Parent)
 //? Asagi yonde (childeren)
 //? Yatay yonde (sibling)
+
+//* 1- Yukari yonde traverse
+
+const addItem = document.querySelector(".add-item")  //* claslar (.)  ile secilir.
+
+console.log(addItem)
+
+console.log(addItem.parentElement) //? main
+console.log(addItem.parentNode.parentNode) //? body
