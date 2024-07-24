2e2f8a655ee6a396c842e3d8a9722dd5319d3b4b
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:13:41 +0200

    filter map methods

--- js10_objets/app.js
+++ js10_objets/app.js
@@ -1,3 +1,18 @@
  //**  Reduce methods */
 
- 
+ XMLDocument.forEach (( value, item, array) => {
+    arr [] = value * 2
+ })
+
+ //* for each  return ozelligi yok , 
+ //* bir dizi icersinde doneriz  value zorunlu item ve array optional  bize array dondurur
+
+
+//  * arr = [8,9,10,2]
+// * const newArr = arr.map((x) => x  * x)    map return ozelligi var
+
+//* arr. filter (( v, i,a) =>  condition)
+
+//* grader [55, 23, 44, 48, 50, 90]
+
+//* const bigger50  = grades.filter((g) => g>=50)
