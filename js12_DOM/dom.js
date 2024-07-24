e95375d76dec82a3ce9201dc1d8afdc446b5b753
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:19:41 +0200

    DOM Document Objects Model

---
+++ js12_DOM/dom.js
@@ -0,0 +1,12 @@
+ //?   Dom DOCUMENT OBJECT MODEL
+
+ //? API   client ile server arasinda calisan sistem  
+ //? Aplication programering interface
+ //? UI yoktur
+ //? DOM BIR API DIR  BORWSERLAR  ORNEGIN DOM-API DIR 
+ //? API ARA KATMANDIR   DOM API SI OBJE ILE CALISIR  HTML ELEEMANLARININ NESNELER OBJECT  PROPERTIES METHODSLAR EVENTS TANIMLAR
+
+ //* Dom Objects- Properties, Methods, Eventler tanimlar
+
+
+ 
