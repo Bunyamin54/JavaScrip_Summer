9e0a51d8437c8dc9c6dd01bf7cf7e159ba824821
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 15:41:26 +0200

    typeof  javaScript

--- js01-intro/app.js
+++ js01-intro/app.js
@@ -2,14 +2,39 @@
 
 console.log("App JS Running")
 const pi = 3.14
+console.log(pi)
+console.log(typeof pi)
 
 
-alert("Bu bir alerttir") //? alert uygulamayi bloklar
+// alert("Bu bir alerttir") //? alert uygulamayi bloklar
 
-karar = confirm ("Emin misiniz E/H")
+// karar = confirm ("Emin misiniz E/H")
 
-console.log(karar)
+// console.log(karar)
 
-console.warn("Bu bir uyaridir")
+// console.warn("Bu bir uyaridir")
 
-console.error("Eyvah hata olustu")
+// console.error("Eyvah hata olustu")
+
+const sayi1= "3"
+console.log(sayi1)
+console.log(typeof sayi1)
+
+//! const ile atatnan degiskene baslangic degeri verilmelidir ve sonradan atama yapilamaz. 
+
+let language = "Java"
+console.log(language, typeof language)
+
+language = "JavaScript"
+
+console.log(language)
+
+language= true
+console.log(language)
+
+language=3.14
+console.log(language)
+
+let adet
+
+console.log(adet)
