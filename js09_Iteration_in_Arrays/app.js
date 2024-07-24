e71e425e5a474ac826786a75a8abd09b8fed5f31
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:10:00 +0200

    iteretion with for loop

--- js09_Iteration_in_Arrays/app.js
+++ js09_Iteration_in_Arrays/app.js
@@ -1,3 +1,15 @@
 //* Loops white iterations **** 
 
 //*  Array.forEach()  Array.map() Array.filter() Array.reduse() Array.find ()  Array.every()  Array 
+
+const rakamlar = [-5, 15, 22, -4, 45, 78, -25]
+
+let toplam = 0
+for (let i = 0 ; i<rakamlar.length; i++) {
+
+    toplam += rakamlar[i];
+
+}
+
+console.log(toplam) //* 126
+
