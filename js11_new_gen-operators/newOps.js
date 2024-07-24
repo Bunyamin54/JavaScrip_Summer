f070db762bf9c3cfd18952957125c8d40e23a9c5
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:15:45 +0200

    new operations

--- js11_new_gen-operators/newOps.js
+++ js11_new_gen-operators/newOps.js
@@ -4,3 +4,21 @@
   console.log("*** New Operators ****")
 
   //? JSON - JavaScript Object Notation 
+  //? JSON - Bir Arraydir. 
+
+  //? objeyi acma parcalama
+
+  const car = {
+      brand:  "BMW",
+      model:  2020,
+      engine: 2000,
+      colors : ["pink", "blue"],
+    
+  }
+  //?  1. yontem . notasyonu ile ulasim 
+
+  console.log(car.brand)
+
+  //? 2. yontem Square  []  koseli parentez ile ulasim
+
+    console.log(car["model"])
