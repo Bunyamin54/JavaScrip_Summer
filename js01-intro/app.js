cd8838ce59e545873cdfd512d4e48d04f4ddd499
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 15:41:13 +0200

    alert console

--- js01-intro/app.js
+++ js01-intro/app.js
@@ -2,3 +2,14 @@
 
 console.log("App JS Running")
 const pi = 3.14
+
+
+alert("Bu bir alerttir") //? alert uygulamayi bloklar
+
+karar = confirm ("Emin misiniz E/H")
+
+console.log(karar)
+
+console.warn("Bu bir uyaridir")
+
+console.error("Eyvah hata olustu")
