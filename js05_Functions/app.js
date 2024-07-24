32de9fc08798256a9e2a11bb74f0159ff681ecfa
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 15:48:32 +0200

    parametrik structure

--- js05_Functions/app.js
+++ js05_Functions/app.js
@@ -15,10 +15,22 @@
 
 //! Decalaration -  Fonksiyonun tanimlanmasi
 
-function yazdirMerhaba() {
-    // alert("Merhaba FS-14")
-    console.log("Merhaba FS-14")
+// function yazdirMerhaba() {
+//     // alert("Merhaba FS-14")
+//     console.log("Merhaba FS-14")
+// }
+
+
+// yazdirMerhaba() //*  invoke - veya call fonksiyonun cagrilmasi 
+
+
+//? parametrik functions
+
+function yazdirParametre (cohortName) {
+
+
+    console.log("Merhaba", cohortName)
 }
 
-
-yazdirMerhaba() //*  invoke - veya call fonksiyonun cagrilmasi 
+yazdirParametre("FS-15")
+yazdirParametre("FS-16")
