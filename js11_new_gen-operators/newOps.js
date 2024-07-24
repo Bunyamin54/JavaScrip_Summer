6f8621533806cce3ed08662894dc6f4dd345e8cc
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:15:48 +0200

    destructurings methods

--- js11_new_gen-operators/newOps.js
+++ js11_new_gen-operators/newOps.js
@@ -16,9 +16,24 @@
     
   }
   //?  1. yontem . notasyonu ile ulasim 
-
+  const araba =car.brand  //* Degiskene aktarma
   console.log(car.brand)
+  console.log(araba)
 
-  //? 2. yontem Square  []  koseli parentez ile ulasim
+
+  //? 2. yontem Square  brcket  []  koseli parentez ile ulasim
 
     console.log(car["model"])
+
+
+    //? 3 methods. Destructure  parcalama bozma
+
+
+  const {brand, engine,colors } = car
+
+  console.log(engine, colors, name) //* Degiskenlerin isimi key lerin ismi ile ayni olmazi lazm   //? Destructuring object
+
+
+  const {model} = car  //* destructuring methods
+
+  console.log(model)
