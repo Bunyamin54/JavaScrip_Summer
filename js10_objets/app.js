016d45a1bc1671059c6adb978f222a041cf1f187
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:13:45 +0200

    reduce methods

--- js10_objets/app.js
+++ js10_objets/app.js
@@ -1,11 +1,11 @@
  //**  Reduce methods */
 
- XMLDocument.forEach (( value, item, array) => {
+ x.forEach (( value, item, array) => {
     arr [] = value * 2
  })
 
  //* for each  return ozelligi yok , 
- //* bir dizi icersinde doneriz  value zorunlu item ve array optional  bize array dondurur
+ //* bir dizi icersinde doneriz  value zorunlu item ve array optional  bize array dondurur. forEach bitirme islemi yapar. 
 
 
 //  * arr = [8,9,10,2]
@@ -15,4 +15,13 @@
 
 //* grader [55, 23, 44, 48, 50, 90]
 
-//* const bigger50  = grades.filter((g) => g>=50)
+//* const bigger50  = grades.filter((g) => g>=50)'
+
+//* filter bir dizi alip icinde dolasirirz.
+
+//* pipline -chain  chain ozelliginin olmasi icin methodun return ozelligi olmalidir foreach methodu void methodudur
+
+
+//* Reduce (Terminal)
+//* Reduce -> Accumulative islemler yapar birikimli ardisik islem yapar
+//* 
