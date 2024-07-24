cbb03a375927eccbbadb4237f2c2abceb528fec5
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:10:08 +0200

    array.find

--- js09_Iteration_in_Arrays/app.js
+++ js09_Iteration_in_Arrays/app.js
@@ -5,7 +5,7 @@
 const rakamlar = [-5, 15, 22, -4, 45, 78, -25]
 
 let toplam = 0
-for (let i = 0 ; i<rakamlar.length; i++) {
+for (let i = 0 ; i<rakamlar.length; i++) { //* <= dersek 7. elemani dahil ederiz  dinamik kod  // Array.length propertisi ile . ile ulasiriz dinamik yapariz
 
     toplam += rakamlar[i];
 
