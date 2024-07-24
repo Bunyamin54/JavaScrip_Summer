02bb4dbe760561e4d80f5d46486b7d7a7264b63a
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:14:35 +0200

    objects

--- js10_objets/objects.js
+++ js10_objets/objects.js
@@ -1,2 +1,26 @@
 
   console.log("*********  Objects ********")
+
+  //* Komplex veri yapisi  --  property(key) - value  
+
+  //* objeye erisism . notasyonu iel erisilir ---
+
+  //? Object Literal ( En cok tercih edilen yontem )
+  const car =  {
+     
+     brand: "Mercedes",
+     model:2020,
+     speed:5,
+     color: ["pink", "black", "blue", "green"]
+  }
+
+
+   //? Objenin okunmasi   ( read)
+
+   console.log(car.brand)  //* . notasyonu
+
+   console.log(car.color[0]) //? pink
+
+   console.log(car) //! suslu ile gosterilirse objedir. 
+
+   console.log(car.color)
