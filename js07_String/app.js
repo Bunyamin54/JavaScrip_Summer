e4b96605f750da61af05742caeb1d6889effaabf
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:00:38 +0200

    iterasyon

--- js07_String/app.js
+++ js07_String/app.js
@@ -1,10 +1,22 @@
 console.log("*** Strings *** ")
 
 
-let str1 = " This is a string"
+// let str1 = " This is a string"
 
-console.log(typeof str1)
+// console.log(typeof str1)
 
-let str2 = "Clarusway"
+// let str2 = "Clarusway"
 
-console.log(str2.length)
+// console.log(str2.length)
+
+const str = " Hello World"
+
+
+console.log(str[6])
+
+
+for ( let i= 0 ; i < str.length; i++) {
+
+     console.log(str[i])
+
+}
