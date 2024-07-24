c8478a96a5ab5ddbe9139ca6a6c11253690a711a
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:11:23 +0200

    foreach methods

--- js09_Iteration_in_Arrays/iterasjon.js
+++ js09_Iteration_in_Arrays/iterasjon.js
@@ -3,3 +3,17 @@
 //* Iterasjon   Array.forEach  objenin icindeki array elementi icin kullanilan methods  fonksiyon icersidne fonksiyon yazma 
 
 //? callback funkcion
+
+//* Foreach
+
+
+const prices = [ 250, 150, 300, 500]
+
+
+for (let i=0; i< prices.length; i++) {
+    console.log(prices[i])
+}
+  
+
+ console.log("***********")
+prices.forEach( p => console.log(p) )
