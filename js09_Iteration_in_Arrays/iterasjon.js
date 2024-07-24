21f133df638c9444bb9fb264bcb5d0810a3dcc8a
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:11:42 +0200

    Array.Methods forEach

--- js09_Iteration_in_Arrays/iterasjon.js
+++ js09_Iteration_in_Arrays/iterasjon.js
@@ -4,7 +4,7 @@
 
 //? callback funkcion
 
-//* Foreach  -orjinal diziyi degistirmez 
+//* Foreach  -orjinal diziyi degistirmez  callback function alir 3 paramter alir 1. si deger 2. paramtere indisi. 3. paramtere arrayi gosterir  forEach sadece dizilerde kullanilir  array olmayan yerde dizi kullanilamaz
 
 
 const prices = [ 250, 150, 300, 500]
