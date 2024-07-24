197460966514a573533f1939d7bd7dc073408e41
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:10:12 +0200

    for loops

--- js09_Iteration_in_Arrays/app.js
+++ js09_Iteration_in_Arrays/app.js
@@ -2,14 +2,25 @@
 
 //*  Array.forEach()  Array.map() Array.filter() Array.reduse() Array.find ()  Array.every()  Array 
 
-const rakamlar = [-5, 15, 22, -4, 45, 78, -25]
+// const rakamlar = [-5, 15, 22, -4, 45, 78, -25]
 
-let toplam = 0
-for (let i = 0 ; i<rakamlar.length; i++) { //* <= dersek 7. elemani dahil ederiz  dinamik kod  // Array.length propertisi ile . ile ulasiriz dinamik yapariz
+// let toplam = 0
+// for (let i = 0 ; i<rakamlar.length; i++) { //* <= dersek 7. elemani dahil ederiz  dinamik kod  // Array.length propertisi ile . ile ulasiriz dinamik yapariz
 
-    toplam += rakamlar[i];
+//     toplam += rakamlar[i];
 
+// }
+
+// console.log(toplam) //* 126
+
+
+
+const grades = [55, 77, 23, 89, 100, 44, 33]
+
+let sum = 0
+
+for (let i=0 ; i<grades.length; i++) {
+
+
+     sum += grades[i]
 }
-
-console.log(toplam) //* 126
-
