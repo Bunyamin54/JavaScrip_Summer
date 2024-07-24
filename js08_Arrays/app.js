410ba31cf9f83cf8d2759ba17e791ebe0f4cd7eb
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:06:24 +0200

    array with const

--- js08_Arrays/app.js
+++ js08_Arrays/app.js
@@ -78,3 +78,20 @@
 // a = 6
 
 // console.log(a)
+
+//*  non primitivler
+
+let arr1 = [1, 2, 'Threee', false]
+
+const arr2 = arr1
+
+
+console.log(arr1)
+
+console.log(arr2)
+
+arr1[0]= 0
+
+console.log(arr1)
+
+console.log(arr2)
