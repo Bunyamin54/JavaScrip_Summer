faec0e84c4e1c44c38bfccebb5ffcfb06fe061ed
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 15:43:17 +0200

    2. methods

--- js03_conditions/app.js
+++ js03_conditions/app.js
@@ -63,4 +63,8 @@ if ( n1 > n2){
 
 if (n1 >  n2 && n1 > n3) {
     console.log("Biggest:", n1)
+}else if(n2 > n1 && n2> n3) {
+    console.log("Biggest:", n2)
+}else if (n3 > n1 && n3 > n2){
+    console.log("Biggest:" , n3)
 }
