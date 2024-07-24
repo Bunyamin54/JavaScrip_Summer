ffe7087307fe3d06a2602a3566e3e47f2a4aa7f5
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 15:48:15 +0200

    invoke call

--- js05_Functions/app.js
+++ js05_Functions/app.js
@@ -2,3 +2,23 @@
  // ********** Functions **********
 
  console.log(" *** Functions *** ")
+
+
+  // ! Function turleri
+
+ //* function declaration    
+ //* function XPathExpression
+ //* Arrow function
+
+
+//*  camleCase yazmalyiz
+
+//! Decalaration -  Fonksiyonun tanimlanmasi
+
+function yazdirMerhaba() {
+    alert("Merhaba FS-14")
+}
+
+
+yazdirMerhaba() //*  invoke - veya call fonksiyonun cagrilmasi 
+yazdirMerhaba()
