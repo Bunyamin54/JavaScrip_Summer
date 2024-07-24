903d214e493bd94ea8cd5fd0df8656a937f802e8
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:20:10 +0200

    GetElementByTag

--- js12_DOM/dom.js
+++ js12_DOM/dom.js
@@ -62,3 +62,22 @@
     const myInput =  document.getElementsByTagName("input")  //* html collection  -- etiketleri donduruyor-  arrayler grubu donduruyor- dizimsi bir grup 
 
     console.log(myInput)
+
+
+     //* Arraye cevrilebilir
+
+     const myList = document.getElementsByTagName("li") 
+
+     const myListArr = [...myList]  //? Arraye cevirdik 
+
+     console.log(myListArr)
+
+     myListArr.forEach((li) => (li.style.color = "red"))
+
+     //? Alternatif methods  --- objenin propertisini valuesini degistirdik. 
+
+     const myListArr1= Array.from(myList) //?Arraye cevrir
+
+     console.log(myListArr1)
+
+     myListArr1.map((li) => (li.style.listStyleType = "none"))
