49b3a9b6d6ff7152ce3e36dd91ffeb811cf73b81
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 15:49:49 +0200

    arrow otmatik return

--- js05_Functions/expression.js
+++ js05_Functions/expression.js
@@ -27,9 +27,28 @@
 //* Arrow Methods
 
 
-const kareAl = (num) => num * num
+// const kareAl = (num) => num * num
 
-const topla =(s1 , s2) => s1 + s2
+// const topla =(s1 , s2) => s1 + s2
 
-console.log("KARE", kareAl(4))
-console.log("TOPLA", topla(4, 5))
+// console.log("KARE", kareAl(4))
+// console.log("TOPLA", topla(4, 5))
+
+//*  ornek   3 sayinin en buyugu bulan fonks
+
+  const findBiggest = (n1, n2, n3) => {
+
+  let biggest = n1
+  if(n2 >= biggest) {
+    biggest = n2
+  }
+
+  if (n3 >= biggest) {
+    biggest = 3
+  }
+    return biggest
+  }
+
+  console.log("BIGGEST", findBiggest(1, 2, 3))
+
+  //! Arrow functionlarda suslu acilirsa otamatik return ozelligi kalkar
