4c8f49052d9a2ca4a957a2dfcb2a8e6cae183b64
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 22:21:39 +0200

    Traversing DOm

---
+++ js12_DOM/traversingDOM.js
@@ -0,0 +1,12 @@
+
+
+console.log("Traversing DOM")
+
+//* Traversing DOM 
+
+//? bir html elementini uzerinden baska elementleri secemeye Dom traversing denilir. 
+//? Secme islemi 3 yonlu yapilir   
+
+//? Yukari yonde (Parent)
+//? Asagi yonde (childeren)
+//? Yatay yonde (sibling)
