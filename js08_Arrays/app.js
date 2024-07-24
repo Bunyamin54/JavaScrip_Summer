315dee89d9a5911c083b4b8f853017cab00fadbf
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:05:49 +0200

    literal methods

--- js08_Arrays/app.js
+++ js08_Arrays/app.js
@@ -4,4 +4,10 @@
 
  const arr = [1,2, "true", 3]  
 
- //* literel methods  const arr = [100, "clarusway", true]
+ //* literel methods  const arr = [100, "clarusway", true]  modern methods 
+
+ //* with new keyword   
+
+ const dizi = new Array (100, 'CW', true)
+
+ console.log(dizi)
