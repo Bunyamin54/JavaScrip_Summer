25b130d9c7eabeeb7c57300db3267502f6bcfbd8
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-24 16:15:05 +0200

    Object structure

--- js10_objets/objects.js
+++ js10_objets/objects.js
@@ -1,32 +1,32 @@
 
   console.log("*********  Objects ********")
 
-  //* Komplex veri yapisi  --  property(key) - value  
+//   //* Komplex veri yapisi  --  property(key) - value  
 
-  //* objeye erisism . notasyonu iel erisilir ---
+//   //* objeye erisism . notasyonu iel erisilir ---
 
   //? Object Literal ( En cok tercih edilen yontem )
-  const car =  {
+//   const car =  {
      
-     brand: "Mercedes",
-     model:2020,
-     speed:5,
-     color: ["pink", "black", "blue", "green"],
-     engine: 2000
-  }
+//      brand: "Mercedes",
+//      model:2020,
+//      speed:5,
+//      color: ["pink", "black", "blue", "green"],
+//      engine: 2000
+//   }
 
 
    //? Objenin okunmasi   ( read)
 
-   console.log(car.brand)  //* 1.yontem method . notasyonu
+//    console.log(car.brand)  //* 1.yontem method . notasyonu
 
-   console.log(car.color[0]) //? pink
+//    console.log(car.color[0]) //? pink
 
-   console.log(car) //! suslu ile gosterilirse objedir. 
+//    console.log(car) //! suslu ile gosterilirse objedir. 
 
-   console.log(car.color)
+//    console.log(car.color)
 
-   car.color.forEach((c)=> console.log(c))  //* diziyi donduk  
+//    car.color.forEach((c)=> console.log(c))  //* diziyi donduk  
 
 
    //* key case sensetive yapilardir. 
@@ -34,11 +34,11 @@
 
    //? Obje degerinin degistirilmesi (write)  ekleme yazma yapiyoruz
 
-   car.speed = 7  //? writing  
-   car.color ="Morcivert"
+//    car.speed = 7  //? writing  
+//    car.color ="Morcivert"
 
-   car.class = "E"
-   console.log(car)
+//    car.class = "E"
+//    console.log(car)
 
 
    //! Objeler none primitive yapidadir.   stack - heap 
@@ -48,7 +48,7 @@
    //* 2. yontem  Square Bracket yontemi ile erisim   objenin icindeki degiskenlereulasiyoruz  bu yotem ile  -- degisken yazmamiza izin veriyor, dinamik olarak obje erisimlerinde kullanilabilir. 
 
 
-    console.log("ENGINE:" , car["engine"])
+    // console.log("ENGINE:" , car["engine"])
 
     //   const myKey = prompt( "Araba ile ilgili neyi ogrenmek istersiniz: speed, color, engine ....")
 
@@ -60,24 +60,46 @@
 
       const myCar = new Object()  //* new operatoru ile  new Key word Object classi   object clasi temel objeden  myCar uretmis olduk
 
-      myCar.brand = "Audi"
-      myCar.model = 2022
-      myCar.speed = 8
+    //   myCar.brand = "Audi"
+    //   myCar.model = 2022
+    //   myCar.speed = 8
 
-      console.log(myCar.speed)
-      console.log(myCar["model"])
+    //   console.log(myCar.speed)
+    //   console.log(myCar["model"])
 
-      //*  Object constructir u kullanrak 
+    //   //*  Object constructir u kullanrak 
 
-      function Personel(id, name, age, salary) {
+    //   function Personel(id, name, age, salary) {
 
-     this.id = id 
-     this.age = age
-     this.name = name
-     this.salary = salary
-      }
+    //  this.id = id 
+    //  this.age = age
+    //  this.name = name
+    //  this.salary = salary
+    //   }
 
-      const person1 = new Personel ("1234567", "Ahmet Can", 33, 30000)
-      const person2 = new Personel ("1234567", "Mehmet Can", 30, 25000)
+    //   const person1 = new Personel ("1234567", "Ahmet Can", 33, 30000)
+    //   const person2 = new Personel ("1234567", "Mehmet Can", 30, 25000)
 
-      console.log(person1)
+    //   console.log(person1)
+
+
+       //? Object methods
+
+
+       const personel = {
+
+
+         name: "Ahmet",
+         surname: "Canan",
+         dob: 1990,
+         salary: 30000,
+         drivingLicense: true,
+
+         calculateAge: function() {
+
+            return new Date().getFullYear() - dob
+
+         }
+       }
+
+       console.log(personel.name + " " + personel.surname)
