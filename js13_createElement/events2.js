e734cd57c38b061654c72880f974d196ff5fbd63
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-25 21:32:17 +0200

    window.onload = printHello()

--- js13_createElement/events2.js
+++ js13_createElement/events2.js
@@ -3,3 +3,18 @@ console.log("*** Events ***")
 //* Onclick , Onload, Onmouseover, Onmouseout, Onchange, Onsubmit,Onkeyup
 
 //* Inline Events
+
+const heading1 = document.querySelector("header h1")
+
+heading1.onmouseover = function () { 
+  heading1.style.color = "red"
+} 
+
+
+heading1.onmouseout= function() {
+    heading1.style.color = "black"
+}
+
+//* onload event i tum html ve css kodlarinin yuklenmesini bekler
+
+window.onload = printHello()
