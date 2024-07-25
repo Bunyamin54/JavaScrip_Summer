b57338874ad50d4fafaa7b3558b8c6b177d3167e
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-25 13:01:55 +0200

    Element.before() after

--- js13_createElement/events.js
+++ js13_createElement/events.js
@@ -1,16 +1,28 @@
- console.log("*** Crate Node Events ***")
+ console.log("*** Crate Node Events ***")  //* interup mantigi trigger tetikleyici- 
 
 
 const langInput = document.querySelector("#input")
- //* Created  - olusturma
+ //* 1- Created  - olusturma
 const newP =document.createElement("p")
-//* Eger text tabanli bir element ise text dugumunu olutur
+//* 2- Eger text tabanli bir element ise text dugumunu olutur
 // const text = document.createTextNode("Bu bir yeni p elementidir.")
 const text = document.createTextNode("langInput.value")
 
-//* Olusturulan text elementini yeni elemente bagla
+//*3- Olusturulan text elementini yeni elemente bagla
 
  newP.appendChild(text)
 
  console.log(newP)
- //* Bagla (append)  
+ //* 4- Bagla (append)   yeni elementin baglanmasi
+
+ 
+ const main = document.querySelector("main")
+
+  console.log(main)
+ main.appendChild(newP)
+
+  //* item-list sectionun onune yeni elementi ekler
+  //* Element.before () ve Element.after () appendChild gibi aslinda bir elementi DOM Treeye baglar. 
+ const itemListSection = document.querySelector(".item-list")
+
+ itemListSection.before(newP)
