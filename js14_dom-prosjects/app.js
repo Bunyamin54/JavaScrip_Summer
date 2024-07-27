0bb7ee9f4ae75d3db8bebb149c161275b11225f9
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-27 14:14:49 +0200

    draw

--- js14_dom-prosjects/app.js
+++ js14_dom-prosjects/app.js
@@ -2,15 +2,31 @@
  
  //* Selectors
   const selectionArticle= document.querySelector(".selection")
+  const yourChoiceDiv =  document.getElementById("your-choice") 
+  const pcChoiceDiv= document.querySelector("pc-choice")
 
 
 
  //* Variables
 
- let userSelectorImg = document.createElement("img")
-const yourChoiceDiv =  document.getElementById("your-choice") 
+ let userSelectImg = document.createElement("img")
+ let pcSelectImg = document.createElement("img")
 
 
+ //? colors
+
+ const YELLOW ="#ffc538"
+ const RED ="#5ab7ac"
+ const GREEN ="#5ab7ac"
+
+//* massage
+
+const messagePar= document.querySelector(".message")
+
+
+//* Score
+
+const scoreCardSection = document.querySelector(".score-card")
 
  //* Event Listeners
 
@@ -20,13 +36,41 @@ const yourChoiceDiv =  document.getElementById("your-choice")
   console.log(e.target.id)
 
    if(e.target.id) {
-    userSelectorImg.src =` ./assets/${e.target.id}.png`
-    userSelectorImg.alt= e.target.id 
-    yourChoiceDiv.appendChild(userSelectorImg) 
+    userSelectImg.src =` ./assets/${e.target.id}.png`
+    userSelectImg.alt= e.target.id 
+    yourChoiceDiv.appendChild(userSelectImg) 
+    createPcSelection ()
 
-   }
+   } 
+})
+     //* Functions 
 
-  })
+   const createPcSelection = () => {
+   const pcArr = ["rock", "paper", "scissor"]
+   const pcRandom = pcArr[Math.floor(Math.random() * 3)]
+   pcSelectImg.src = `./assets/${pcRandom}.png`
+   pcSelectImg.alt = pcRandom   
+   pcChoiceDiv.appendChild(pcSelectImg)
+
+   calculateResult()
+
+}
+      
+
+     const calculateResult = () => {
+     
+         if(userSelectImg.alt === pcSelectImg.alt) {
+        draw()
+
+         }
+
+         const draw = () => {
+
+            messagePar.textContent= "Its a draw"
+            scoreCardSection.style.color = YELLOW
+         }
+
+        }
 
 
 
@@ -40,7 +84,7 @@ const yourChoiceDiv =  document.getElementById("your-choice")
 
   //* Event Listeners
 
-  //? Eksempel 1 
+  //? Eksempel 1   Ilkel yontem
 
 //   const rockImg =document.getElementById("rock")
 //   const paperImg =document.getElementById("paper")
@@ -71,4 +115,3 @@ const yourChoiceDiv =  document.getElementById("your-choice")
 //   yourChoiceDiv.appendChild(image) 
 // })
  
- //* Functions 
