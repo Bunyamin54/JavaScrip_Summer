94dd4723ee8aa7d41512bfd514ccd768fc5ee22b
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-27 13:47:38 +0200

    selection Article selectors

--- js14_dom-prosjects/app.js
+++ js14_dom-prosjects/app.js
@@ -1,43 +1,74 @@
  console.log("***Stein-Saks******")
  
  //* Selectors
+  const selectionArticle= document.querySelector(".selection")
 
-  const rockImg =document.getElementById("rock")
-  const paperImg =document.getElementById("paper")
-  const scissorImg =document.getElementById("scissor")
-
- //? Secilen elementlerin tasiyicisi 
-const yourChoiceDiv =  document.getElementById("your-choice")  
 
 
  //* Variables
 
-
-
+ let userSelectorImg = document.createElement("img")
+const yourChoiceDiv =  document.getElementById("your-choice") 
 
 
 
  //* Event Listeners
-  let image = document.createElement("img")
-   rockImg.addEventListener("click", () => {
+
+   
+
+  selectionArticle.addEventListener("click", (e) => {
+  console.log(e.target.id)
+
+   if(e.target.id) {
+    userSelectorImg.src =` ./assets/${e.target.id}.png`
+    userSelectorImg.alt= e.target.id 
+    yourChoiceDiv.appendChild(userSelectorImg) 
+
+   }
+
+  })
+
+
+
+
+
+
+
+
+
+
+
+  //* Event Listeners
+
+  //? Eksempel 1 
+
+//   const rockImg =document.getElementById("rock")
+//   const paperImg =document.getElementById("paper")
+//   const scissorImg =document.getElementById("scissor")
+
+//  //? Secilen elementlerin tasiyicisi 
+// const yourChoiceDiv =  document.getElementById("your-choice")  
  
-  image.src = "./assets/rock.png"
-  image.alt= "rock"
-
-  yourChoiceDiv.appendChild(image) })
-
-   paperImg.addEventListener("click", () => {
+//   let image = document.createElement("img")
+//    rockImg.addEventListener("click", () => {
  
-  image.src = "./assets/paper.png"
-  image.alt= "paper"
+//   image.src = "./assets/rock.png"
+//   image.alt= "rock"
 
-  yourChoiceDiv.appendChild(image) })
+//   yourChoiceDiv.appendChild(image) })
+
+//    paperImg.addEventListener("click", () => {
+ 
+//   image.src = "./assets/paper.png"
+//   image.alt= "paper"
+
+//   yourChoiceDiv.appendChild(image) })
 
 
-  scissorImg.addEventListener("click", () => {
-  image.src = "./assets/scissor.png"
-  image.alt= "scissor"
-  yourChoiceDiv.appendChild(image) 
-})
+//   scissorImg.addEventListener("click", () => {
+//   image.src = "./assets/scissor.png"
+//   image.alt= "scissor"
+//   yourChoiceDiv.appendChild(image) 
+// })
  
  //* Functions 
