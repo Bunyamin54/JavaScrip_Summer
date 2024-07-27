8dbaf82110b7cd3e35f420769865776d0e7b8ac8
“Bunyamin54”~ <bunyaming@uia.no>
2024-07-27 13:28:08 +0200

    created event

--- js14_dom-prosjects/app.js
+++ js14_dom-prosjects/app.js
@@ -1,7 +1,29 @@
+ console.log("***Stein-Saks******")
+ 
  //* Selectors
+
+  const rockImg =document.getElementById("rock")
+  const paperImg =document.getElementById("paper")
+  const scissorImg =document.getElementById("sicissor")
+
+ //? Secilen elementlerin tasiyicisi 
+const yourChoiceDiv =  document.getElementById("your-choice")  
+
 
  //* Variables
 
- //* Event Listeners
 
+
+
+
+
+ //* Event Listeners
+  let image = document.createElement("img")
+   rockImg.addEventListener("click", () => {
+ 
+  image.src = "./assets/rock.png"
+  image.alt= "rock"
+
+  yourChoiceDiv.appendChild(image) })
+ 
  //* Functions 
