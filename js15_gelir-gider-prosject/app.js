dec7e2d96ae66f01fe53bac55b00f35c9a925a81
“Bunyamin54”~ <bunyaming@uia.no>
2024-08-17 15:04:25 +0200

    e.preventdefault

--- js15_gelir-gider-prosject/app.js
+++ js15_gelir-gider-prosject/app.js
@@ -1,76 +1,69 @@
 //? Selectors
 
-const ekleBtn = document.getElementById("ekle-btn")
-const gelirInput = document.getElementById("gelir-input")
-const ekleFormu = document.getElementById("ekle-formu")
+const ekleBtn = document.getElementById ('ekle-btn');
+const gelirInput = document.getElementById ('gelir-input');
+const ekleFormu = document.getElementById ('ekle-formu');
 
-//? Sonuc tablosu 
- const gelirinizTd= document.getElementById("geliriniz")
+//? Sonuc tablosu
+const gelirinizTd = document.getElementById ('geliriniz');
 
+//? harcama formu
 
- //? harcama formu 
-
- const harcamaFormu = document.getElementById("harcama-formu")
- const harcamaAlaniInput = document.getElementById("harcama-alani")
- const tarihInput = document.getElementById("tarih")
- const miktarInput = document.getElementById("miktar")
+const harcamaFormu = document.getElementById ('harcama-formu');
+const harcamaAlaniInput = document.getElementById ('harcama-alani');
+const tarihInput = document.getElementById ('tarih');
+const miktarInput = document.getElementById ('miktar');
 
 //? Variables
 
-let gelirler = 0
-
-
-
+let gelirler = 0;
 
 //? Events
 
-
-
-
 //? formun submit butonuna basildiginda
 
-ekleFormu.addEventListener("submit", (e) => { 
-    
-    e.preventDefault() // reload"u engeller
-    gelirler = gelirler + Number ( gelirInput.value) //? string eklemeyi engelledik 
+ekleFormu.addEventListener ('submit', e => {
+  e.preventDefault (); // reload"u engeller
+  gelirler = gelirler + Number (gelirInput.value); //? string eklemeyi engelledik
 
+  localStorage.setItem ('gelirler', gelirler);
 
-    localStorage.setItem("gelirler", gelirler)
+  //? input degerini sifirladik...
+  ekleFormu.reset ();
 
-    //? input degerini sifirladik... 
-    ekleFormu.reset()
+  //? Degisiklikleri sonuc tablosuna yazan fonksiyon
 
+  hesaplaVeGuncelle ();
+});
 
-//? Degisiklikleri sonuc tablosuna yazan fonksiyon 
+//! harcama formu submit edildiginde calisir
 
+harcamaFormu.addEventListener ('submit', (e) => {
+  e.preventDefault ();
 
+  const yeniHarcama = {
+    id: new Date().getTime(),
+    tarih: tarihInput.value,
+    alan: harcamaAlaniInput.value,
+    miktar: miktarInput.value,
+   
 
-      hesaplaVeGuncelle()
-})
-
-//? harcama formu submit edildiginde calisir
-
-  harcamaFormu.addEventListener("submit", () => {
-
-     e.preventDefault()
-     const yeniHarcama = { id: "" , tarih:"", alan: "", miktar: ""}
-
-
-  })
-
+  };
+  harcamaFormu.reset()
+  tarihInput.valueAsDate = new Date()
+   console.log(yeniHarcama)
+});
 
 //! Sayfa heryuklendikten sonra calisan event
 
-window.addEventListener("load", () => {
+window.addEventListener ('load', () => {
+  gelirler = Number (localStorage.getItem ('gelirler'));
+  tarihInput.valueAsDate = new Date()
 
-  gelirler = Number( localStorage.getItem("gelirler"))
-  hesaplaVeGuncelle()
-   
-})
+  hesaplaVeGuncelle ();
+});
 //? Functions
 
 const hesaplaVeGuncelle = () => {
-
- gelirinizTd.innerText = gelirler
-
-}
+  gelirinizTd.innerText = gelirler;
+};
