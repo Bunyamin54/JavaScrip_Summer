033248add3d7546e2ae43e162bc0c4f35199dece
“Bunyamin54”~ <bunyaming@uia.no>
2024-08-17 13:41:53 +0200

    e.preventDefault()

--- js15_gelir-gider-prosject/app.js
+++ js15_gelir-gider-prosject/app.js
@@ -2,19 +2,27 @@
 
 const ekleBtn = document.getElementById("ekle-btn")
 const gelirInput = document.getElementById("gelir-input")
-
+const ekleFormu = document.getElementById("ekle-formu")
 
 
 //? Variables
 
-
+let gelirler = " "
 
 
 
 
 //? Events
 
+//? formun submit butonuna basildiginda
 
+ekleFormu.addEventListener("submit", (e) => { 
+    
+    e.preventDefault() // reload"u engeller
+    gelirler += gelirInput.value
+
+    console.log(gelirler)
+})
 
 
 
