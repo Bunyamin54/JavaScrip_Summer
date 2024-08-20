614e4868db4f0e2d197b3e39b4a88f457e4a20bc
“Bunyamin54”~ <bunyaming@uia.no>
2024-08-20 14:25:35 +0200

    async and promis structure

---
+++ 2-promise.js
@@ -0,0 +1,15 @@
+//* =================================================
+//*                2- Promises
+//* =================================================
+
+//? Promise, asenkron bir islemin basariyla ve basarisizlikla bittigini gosteren
+//? ve ayni zamanda basariyla bittiginde sonuc verilerini temsil eden bir nesne yapisidir.
+
+//? SYTNAX
+//?----------
+//* 1- Ilk olarak new Promise() constructor'i ile yeni bir promise nesnesi olusturulur,
+//* 2- constructor'a asil islemin yapilmasini saglayan bir executor fonksiyion verilir.
+//* 3- Executor fonksiyona ise 2 argument gecirilir: resolve ve reject fonksiyonlari
+//* 4- resolve fonksiyonu promise'in basariyla bittiginda, reject ise
+//*    basarisizlikla bittiginde isletilirler.
+
